a8fa4c47801fbd81303856af0837d4f7d5eb4862
KaitoMajima <kaitomajima@outlook.com>
2023-09-17 18:52:43 -0300

    chore: setup main menu nodes

--- $Game/Scripts/Core/MainMenu/MainMenuScope.cs
+++ $Game/Scripts/Core/MainMenu/MainMenuScope.cs
@@ -2,8 +2,20 @@
 
 public partial class MainMenuScope : Node
 {
+    #region NodePaths
+    [Export] NodePath mainMenuNodePath;
+    #endregion
+
+    #region Nodes
+    public IMainMenuNode MainMenuNode { get; private set; }
+    #endregion
+
+    #region Scopes
+    GameScope GameScope => GameScope.Instance;
+    #endregion
+    
     public override void _Ready ()
     {
-        
+        MainMenuNode = GetNode<MainMenuNode>(mainMenuNodePath);
     }
 }
